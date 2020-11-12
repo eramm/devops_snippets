ba5a609b41eb766a401950ddc5cf8a1138d13e69
Eliezer Ramm <eliezer.ramm@harman.com>
2020-11-12 14:11:28 +0200

    added sed and SG

--- aws_ec2_query_all_stopped_instances.sh
+++ aws_ec2_query_all_stopped_instances.sh
@@ -1,2 +1,4 @@
-aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query 'Reservations[*].Instances[*].[InstanceId, InstanceType,KeyName, LaunchTime,PrivateIpAddress, PrivateDnsName, PublicDnsName, PublicIpAddress,SecurityGroups,[Tags[?Key==`Name`].Value] [0][0] ]
+aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query 'Reservations[*].Instances[*].[InstanceId, InstanceType,KeyName, LaunchTime,PrivateIpAddress, PrivateDnsName, PublicDnsName, PublicIpAddress,SecurityGroups[*].GroupName,[Tags[?Key==`Name`].Value] [0][0] ]' --output text > my.tsv
 
+
+## use sed to get everything on one line sed -i '$!N;s/\n/\t/' my.tsv
